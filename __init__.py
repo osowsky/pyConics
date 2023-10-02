b60738c8b3d6670dadeee5f0682a681f5eb0df89
Jefferson Osowsky <jeff.osowsky@hotmail.com>
2023-10-02 19:10:04 -0300

    __init__ dunder: Define the pyConics package.

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#------------------------------------------------------------------
+# __init__ dunder.
+#
+# It defines a package in Python.
+#
+
+#------------------------------------------------------------------
+# Modules that belong to pyConics package.
+#
+from .origin import *
+from .point import *
+from .line import *
+from .functions import *
