d20fa5e367f99a00cda1adf8b714c53e5973212b
Jefferson Osowsky <jeff.osowsky@hotmail.com>
2023-10-05 17:22:38 -0300

    Point Class: Fixed bugs.

--- point.py
+++ point.py
@@ -147,33 +147,33 @@ if __name__ == '__main__':
     # How to use cross product.
     p1 = Point( ( 1, 1 ) )      # p1 = ( 1, 1 )
     p2 = Point( ( -1, -1 ) )    # p2 = ( -1, -1 ) 
-    l1 = p1.cross( p2 )         # l1: y = x
+    l1: Line = p1.cross( p2 )   # l1: y = x
     print( l1, '\n' )
-    l1 = p1 * p2                # l1: y = x
+    l1: Line = p1 * p2          # l1: y = x
     print( l1, '\n' )
     
     l1 = Line( ( 1, -1, 1 ) )
     l2 = Line( ( 1, -1, -1 ) )
     l3 = Line( ( -1, -1, 1 ) )
-    p3 = l1.cross( l2 )       # p3 is a point at the infinity.
+    p3: Point = l1.cross( l2 )  # p3 is a point at the infinity.
     print( p3, '\n' )
-    p3 = l1 * l2       # p3 is a point at the infinity.
+    p3: Point = l1 * l2         # p3 is a point at the infinity.
     print( p3, '\n' )
-    p4 = l1.cross( l3 )       # p4 = ( 0, 1 )
+    p4: Point = l1.cross( l3 )  # p4 = ( 0, 1 )
     print( p4, '\n' )
-    p4 = l1 * l3       # p4 = ( 0, 1 )
+    p4: Point = l1 * l3         # p4 = ( 0, 1 )
     print( p4, '\n' )
-    p5 = l2.cross( l3 )       # p5 = ( 1, 0 )
+    p5: Point = l2.cross( l3 )  # p5 = ( 1, 0 )
     print( p5, '\n' )
-    p5 = l2 * l3       # p5 = ( 1, 0 )
+    p5: Point = l2 * l3         # p5 = ( 1, 0 )
     print( p5, '\n' )
 
     p6 = Point( ( 1, 0 ) )
-    l4 = l1.cross( p6 )        # l4 = ( 1, 1, -1 ) that pass through p6
+    l4: Line = l1.cross( p6 )  # l4 = ( 1, 1, -1 ) that pass through p6
     print( l4, '\n' )
-    l4 = l1 * p6        # l4 = ( 1, 1, -1 ) that pass through p6
+    l4: Line = l1 * p6         # l4 = ( 1, 1, -1 ) that pass through p6
     print( l4, '\n' )
-    l5 = p6.cross( l1 )        # l5 = ( 1, 1, -1 ) that pass through p6
+    l5: Line = p6.cross( l1 )  # l5 = ( 1, 1, -1 ) that pass through p6
     print( l5, '\n' )
-    l5 = p6 * l1        # l5 = ( 1, 1, -1 ) that pass through p6
+    l5: Line = p6 * l1         # l5 = ( 1, 1, -1 ) that pass through p6
     print( l5, '\n' )
