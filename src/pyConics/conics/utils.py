57b562f7bb7f21981bf6a60bdf3e85bc9c4ea41d
Jefferson Osowsky <jeff.osowsky@hotmail.com>
2023-11-07 13:48:04 -0300

    create_conic_from_lines method was created.

--- src/pyConics/conics/utils.py
+++ src/pyConics/conics/utils.py
@@ -6,8 +6,7 @@ from __future__ import annotations
 # #------------------------------------------------------------------
 # # Everything that can be visible to the world.
  
-# __all__ = [ 'skew_symmetric', 'cross', 'dot', 'are_perpendicular', \
-#             'are_parallel', 'distance' ]
+__all__ = [ 'create_conic_from_lines' ]
 
 # #------------------------------------------------------------------
 # # Import from...
@@ -29,9 +28,52 @@ from __future__ import annotations
 # from pyConics.tolerance import tol
 # from pyConics.constants import const
 # from pyConics.point import CPoint
-# from pyConics.line import CLine
+from pyConics.line import CLine
 
 # #------------------------------------------------------------------
 # # Import as...
 # #
-# import numpy as np
+import numpy as np
+
+def create_conic_from_lines( lines: tuple[ CLine, CLine ] ) -> np.ndarray:
+    l1 = lines[ 0 ].gform[np.newaxis]
+    l2 = lines[ 1 ].gform[np.newaxis].T
+    M = ( l2 @ l1 )
+    return M + M.T
+
+#------------------------------------------------------------------
+# For development and test.
+#  
+if __name__ == '__main__':
+    # Keep this imports even there is no test code.
+    from pyConics.point import CPoint
+    from pyConics.line import CLine
+    from pyConics.conics.conic import CConic
+
+    # Degenerate conic.
+    f = CLine( ( 1, 0, 1 ) )    
+    g = CLine( ( 1, 0, -1 ) )
+    C0 = CConic( degenerate = ( f, g ), name = 'C0' )
+    print( C0 )
+    print( f'Is {C0.name} degenerate? {C0.is_degenerate}.\n' )
+
+    # Degenerate conic.
+    f = CLine( ( 1, 0, -1 ) )    
+    g = CLine( ( 1, 0, -3 ) )
+    C1 = CConic( degenerate = ( f, g ), name = 'C1' )
+    print( C1 )
+    print( f'Is {C1.name} degenerate? {C1.is_degenerate}.\n' )
+
+    # Degenerate conic.
+    f = CLine( ( 1, -1, -2 ) )    
+    g = CLine( ( -1, -1, 2 ) )
+    C2 = CConic( degenerate = ( f, g ), name = 'C2' )
+    print( C2 )
+    print( f'Is {C2.name} degenerate? {C2.is_degenerate}.\n' )
+
+    # Degenerate conic.
+    f = CLine( ( 1, 0, -2 ) )    
+    C3 = CConic( degenerate = ( f, f ), name = 'C3' )
+    print( C3 )
+    print( f'Is {C3.name} degenerate? {C3.is_degenerate}.\n' )
+
